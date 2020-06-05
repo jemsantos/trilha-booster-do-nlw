35b653bec7a397e380111d956c8f7ae36e7a1a3b
jemsantos <eustaquio.cadastros@gmail.com>
2020-06-04 23:46:18 -0300

    Versão final do segundo dia do NLW

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,23 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PoitsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
 
 const routes = express.Router();
+const poitsController = new PoitsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
+/* padrões: index, show, create, update delete/destroy */
+routes.get('/items', itemsController.index);
 
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`
-        }
-    });
-
-    return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    });
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id
-        };
-    });
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({ success: true });
-});
+routes.post('/points', poitsController.create);
+routes.get('/points', poitsController.index);
+routes.get('/points/:id', poitsController.show);
 
 export default routes;
+
+/*
+pontos de melhoria aqui....
+Service Pattern
+Repository Pattern (Data Mapper)
+*/
