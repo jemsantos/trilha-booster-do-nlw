7ab3c31b6522b387c4675e69b4e6b6b8f0a1f1a5
jemsantos <eustaquio.cadastros@gmail.com>
2020-06-06 01:05:05 -0300

    Versão final após a última aula contendo somendo o server.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,18 +1,42 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PoitsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const poitsController = new PoitsController();
 const itemsController = new ItemsController();
 
 /* padrões: index, show, create, update delete/destroy */
 routes.get('/items', itemsController.index);
 
-routes.post('/points', poitsController.create);
 routes.get('/points', poitsController.index);
 routes.get('/points/:id', poitsController.show);
+
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required()
+        })
+    }, {
+        abortEarly: false
+    }),
+    poitsController.create);
 
 export default routes;
 
